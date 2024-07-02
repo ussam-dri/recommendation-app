a0466d5e59e79c52c9cab9e0642159ac2ab833ef
Miime <oumaimabourjlate14@gmail.com>
2024-07-02 18:34:53 +0200

    d

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -6,9 +6,8 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <>
-        <Header />
-      </>
+
+      <Header />
       <Homebody />
       <Footer />
     </div>
--- reactjs/src/components/Header.jsx
+++ reactjs/src/components/Header.jsx
@@ -9,14 +9,13 @@ const Header = () => {
     };
     return (
         <div >
-            <a href="" style={{marginright: '25px'}}>Home</a> 
-            <a href="" style={{marginright: '25px'}}>Cafe</a>
-            <a href="" style={{marginright: '25px'}}>Home</a> 
-            <a href="" style={{marginright: '25px'}}>Librairy</a> 
-            <a href="" style={{marginright: '25px'}}>Hotel</a> 
-            <a href="" style={{marginright: '25px'}}>Gym</a> 
-            <a href="" style={{marginright: '25px'}}>My profile</a> 
-            <a href="" style={{marginright: '25px'}}>Contact us</a> 
+            <a href="reactjs\src\components\Home.jsx" >Home</a> 
+            <a href="reactjs\src\components\Cafe.jsx" >Cafe</a>
+            <a href="reactjs\src\components\Librairy.jsx" >Librairy</a> 
+            <a href="reactjs\src\components\Hotel.jsx" >Hotel</a> 
+            <a href="reactjs\src\components\Gym.jsx" >Gym</a> 
+            <a href="reactjs\src\components\Account.jsx" >My profile</a> 
+            <a href="reactjs\src\components\Contact.jsx" >Contact us</a> 
             <h1 onClick={leHeader} className={logo ? 'hidden' : 'block'}>LDApp.</h1>
             <h2 style={{paddingleft: '50px', margin:'100px' , backgroundcolor: '#646824', color: 'white', border: '3px double #D7A14E', width: '80%', maxwidth: '400px'}}>LOCAL.DISCOVERY.APP</h2>
         </div>
