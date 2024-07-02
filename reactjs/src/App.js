3d8949fc5dcb7e0fcc2d0fb52dad2b7969bd9464
Miime <oumaimabourjlate14@gmail.com>
2024-07-02 18:30:49 +0200

    c

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -6,7 +6,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <>
+        <Header />
+      </>
       <Homebody />
       <Footer />
     </div>
--- reactjs/src/components/Header.jsx
+++ reactjs/src/components/Header.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
  
-
 const Header = () => {
+    const [Hed, setHed] = useState(false);
+    const [logo, setLogo] = useState(false)
+    const leHeader = () => {
+        setHed(!Hed);
+        setLogo(!logo);
+    };
     return (
-        <div>
-            <nav style={{ border: '3px double #D7A14E', backgroundcolor: '#D7A14E' ,color:'white' }}>
-                <a href="" style={{marginright: '25px'}}>Home</a> 
-                <a href="" style={{marginright: '25px'}}>Cafe</a> 
-                <a href="" style={{marginright: '25px'}}>Librairy</a>
-                <a href="" style={{marginright: '25px'}}>Hotel</a>
-                <a href="" style={{marginright: '25px'}}>Gym</a>
-                <a href="" style={{marginright: '25px'}}>My profile</a>
-                <a href="" style={{marginright: '25px'}}>Contact</a>
-            </nav>
+        <div >
+            <a href="" style={{marginright: '25px'}}>Home</a> 
+            <a href="" style={{marginright: '25px'}}>Cafe</a>
+            <a href="" style={{marginright: '25px'}}>Home</a> 
+            <a href="" style={{marginright: '25px'}}>Librairy</a> 
+            <a href="" style={{marginright: '25px'}}>Hotel</a> 
+            <a href="" style={{marginright: '25px'}}>Gym</a> 
+            <a href="" style={{marginright: '25px'}}>My profile</a> 
+            <a href="" style={{marginright: '25px'}}>Contact us</a> 
+            <h1 onClick={leHeader} className={logo ? 'hidden' : 'block'}>LDApp.</h1>
             <h2 style={{paddingleft: '50px', margin:'100px' , backgroundcolor: '#646824', color: 'white', border: '3px double #D7A14E', width: '80%', maxwidth: '400px'}}>LOCAL.DISCOVERY.APP</h2>
         </div>
     )
